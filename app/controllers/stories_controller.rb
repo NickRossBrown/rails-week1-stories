53cc7e3a05a613d536c2bb1a0c4a11f50a61622e
Annie Shin and Nick Brown <annie@>
2018-09-05 13:59:31 -0700

    add stories_controller.rb file. Add app/views/stories folder with index.html.erb file

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,23 @@
+class StoriesController < ApplicationController
+
+  def index
+    # @stories = Story.all
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+end
